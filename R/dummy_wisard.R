3f656c977bfeb7dcde096b0f79c37506521f7341
Adharsh Murali <33362260+adharshmurali@users.noreply.github.com>
2019-12-23 15:39:30 -0500

    Support for Group_map functions

--- R/dummy_wisard.R
+++ R/dummy_wisard.R
@@ -28,73 +28,96 @@ dummy_wisard =  function( temporal_data,
 
   # Here we create two frame the first frame is initial frame with summary stats for most recent values.
   # The final frame is a combined frame with first frame and the lagged features frame.
+  
+  print("Entered the dummy wisard function")
+  
+  first_frame = temporal_data %>% 
+    group_by(encounter_id) %>% 
+    mutate(max_time = max(time,na.rm = T)) %>% 
+    ungroup() %>% 
+    group_by(category) %>% 
+    group_map(~check_mapper(.x = .x ,.y = .y)) %>% 
+    bind_rows() 
+  
+  print("Computed the first frame")
+  
+  
+  final_frame = first_frame %>% 
+    group_by(category) %>% 
+    group_map(~lagged_features(.x = .x, .y =.y,recent_only = T)) %>% 
+    bind_rows()
 
-  first_frame = temporal_data %>%
-    dplyr::mutate_at(vars(value), as.numeric) %>%
-    dplyr::filter(time >= 0) %>%
-    dplyr::mutate(actual_time = time,time = floor(time/window_size)*window_size) %>%
-    #group_by(encounter_id) %>%
-    dplyr::mutate(time = factor(time,levels = seq(min(.$time,na.rm =T), max(.$time, na.rm = T),window_size))) %>%
-    dplyr::filter(!is.na(time)) %>%
-    tidyr::complete(encounter_id,variable,time) %>%
-    dplyr::group_by(encounter_id) %>%
-    dplyr::filter( as.numeric(as.character(time)) <= max(actual_time, na.rm = T) )%>%
-    dplyr::ungroup() %>%
-    dplyr::group_by(encounter_id,variable,time) %>%#
-    dplyr::summarise_each( funs(mean,min,max,n = . %>% na.omit %>% length, slope = wisard::slope(actual_time,value)),value) %>%
-    dplyr::ungroup() %>%
-    dplyr::group_by(encounter_id,time) %>%
-    tidyr::gather(key = "key",
-           value = "value",
-           mean:slope) %>%
-    dplyr::mutate(key = case_when( key == "n_distinct" ~ "n", T ~ key)) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(value = case_when( value %in% c(NaN,-Inf,Inf) ~ NA_real_, T ~ value) ) %>%
-    #mutate(time = )
-    dplyr::mutate( time = as.numeric(as.character(time))) %>%
-    dplyr::mutate(time = time - min(time[which(time >= 0)]),lag = min(time))
+  print("Computed the final frame as well!!!")
+  
+  # first_frame = temporal_data %>%
+  #   dplyr::mutate_at(vars(value), as.numeric) %>%
+  #   dplyr::filter(time >= 0) %>%
+  #   dplyr::mutate(actual_time = time,time = floor(time/window_size)*window_size) %>%
+  #   #group_by(encounter_id) %>%
+  #   dplyr::mutate(time = factor(time,levels = seq(min(.$time,na.rm =T), max(.$time, na.rm = T),window_size))) %>%
+  #   dplyr::filter(!is.na(time)) %>%
+  #   tidyr::complete(encounter_id,variable,time) %>%
+  #   dplyr::group_by(encounter_id) %>%
+  #   dplyr::filter( as.numeric(as.character(time)) <= max(actual_time, na.rm = T) )%>%
+  #   dplyr::ungroup() %>%
+  #   dplyr::group_by(encounter_id,variable,time) %>%#
+  #   dplyr::summarise_each( funs(mean,min,max,n = . %>% na.omit %>% length, slope = wisard::slope(actual_time,value)),value) %>%
+  #   dplyr::ungroup() %>%
+  #   dplyr::group_by(encounter_id,time) %>%
+  #   tidyr::gather(key = "key",
+  #          value = "value",
+  #          mean:slope) %>%
+  #   dplyr::mutate(key = case_when( key == "n_distinct" ~ "n", T ~ key)) %>%
+  #   dplyr::ungroup() %>%
+  #   dplyr::mutate(value = case_when( value %in% c(NaN,-Inf,Inf) ~ NA_real_, T ~ value) ) %>%
+  #   #mutate(time = )
+  #   dplyr::mutate( time = as.numeric(as.character(time))) %>%
+  #   dplyr::mutate(time = time - min(time[which(time >= 0)]),lag = min(time))
+  # 
+  # final_frame = NULL
+  # if( lookback > 0 ){
+  #   for ( i in seq(1,(lookback/window_size)-1,1))
+  #   {
+  # 
+  #     final_frame = dplyr::bind_rows( final_frame,
+  #                              first_frame %>%
+  #                                dplyr::group_by(encounter_id,variable,key) %>%
+  #                                dplyr::mutate(new_value = data.table::shift(value,i, type = "lag")) %>%
+  #                                dplyr::mutate(lag = lag + i*window_size) %>%
+  #                                dplyr::ungroup() %>%
+  #                                dplyr::mutate(variable = paste(variable,key,lag, sep = "_")) #%>%
+  #     )
+  #     print(i)
+  #   }
+  # 
+  #   final_frame = dplyr::bind_rows(first_frame %>%
+  #                                    dplyr::mutate(variable = paste(variable,key, lag , sep ="_")) %>%
+  #                                    dplyr::mutate(lag = time),
+  #                           final_frame %>%
+  #                             dplyr::select(encounter_id,variable,time,key,-value,value = new_value, lag)
+  #   )
+  # 
+  #   if(lag){
+  #     lag_frame = wisard::diff_feature(final_frame,
+  #                              window_size)
+  # 
+  #     final_frame = dplyr::bind_rows(final_frame,
+  #                             lag_frame)
+  #     all_variable_to_create = append(all_variable_to_create,lag_variable_to_create)
+  #   }
+  # 
+  # }
+  # else{
+  #   final_frame = first_frame %>%
+  #     dplyr::mutate(variable = paste(variable,key, lag , sep ="_")) %>%
+  #     dplyr::mutate(lag = time)
+  #   print("here")
+  # }
 
-  final_frame = NULL
-  if( lookback > 0 ){
-    for ( i in seq(1,(lookback/window_size)-1,1))
-    {
 
-      final_frame = dplyr::bind_rows( final_frame,
-                               first_frame %>%
-                                 dplyr::group_by(encounter_id,variable,key) %>%
-                                 dplyr::mutate(new_value = data.table::shift(value,i, type = "lag")) %>%
-                                 dplyr::mutate(lag = lag + i*window_size) %>%
-                                 dplyr::ungroup() %>%
-                                 dplyr::mutate(variable = paste(variable,key,lag, sep = "_")) #%>%
-      )
-      print(i)
-    }
-
-    final_frame = dplyr::bind_rows(first_frame %>%
-                                     dplyr::mutate(variable = paste(variable,key, lag , sep ="_")) %>%
-                                     dplyr::mutate(lag = time),
-                            final_frame %>%
-                              dplyr::select(encounter_id,variable,time,key,-value,value = new_value, lag)
-    )
-
-    if(lag){
-      lag_frame = wisard::diff_feature(final_frame,
-                               window_size)
-
-      final_frame = dplyr::bind_rows(final_frame,
-                              lag_frame)
-      all_variable_to_create = append(all_variable_to_create,lag_variable_to_create)
-    }
-
-  }
-  else{
-    final_frame = first_frame %>%
-      dplyr::mutate(variable = paste(variable,key, lag , sep ="_")) %>%
-      dplyr::mutate(lag = time)
-    print("here")
-  }
-
-  if (impute){
+  
+  
+    if (impute){
     #print("I'm here")
     final_frame = final_frame %>%
       dplyr::group_by(encounter_id,variable) %>%
