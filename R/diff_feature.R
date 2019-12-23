b15994760fb90a256490404b923e776ba0e379c7
Adharsh Murali <33362260+adharshmurali@users.noreply.github.com>
2019-12-23 15:42:46 -0500

    Compute proportional variation between lag

--- R/diff_feature.R
+++ R/diff_feature.R
@@ -7,23 +7,31 @@
 #' @return lag frame
 #' @export
 
-diff_feature = function(final_frame,window_size){
+diff_feature = function(final_frame,window_size, lag_compute){
 
+  
   lag_frame = final_frame %>%
     dplyr::group_by(encounter_id,variable) %>%
     # arrange(encounter_id,variable) %>%
     dplyr::mutate(lead_value = shift(value,n =1,type = "lead") ) %>%
-    dplyr::mutate(value = lead_value - value) %>%
+    dplyr::mutate(lag_compute = case_when(lag_compute == "both" ~ "prop/diff", T~ lag_compute)) %>% 
+    separate_rows(lag_compute,convert = T, sep = "/") %>% 
+    dplyr::mutate(new_value = ifelse(lag_compute == "prop",lead_value / value, lead_value - value)) %>% 
     #ungroup() %>%
     dplyr::mutate(lag_time = str_extract_all(variable, "\\d+")) %>%
     tidyr::unnest() %>%
     dplyr::mutate_at(vars(lag_time), as.numeric) %>%
-    dplyr::mutate(lag_time = lag_time - min(lag_time, na.rm = T)) %>%
-    dplyr::filter(lag_time > 0) %>%
+    #dplyr::mutate(lag_time = lag_time - min(lag_time, na.rm = T)) %>%
+    dplyr::filter(lag >= 0) %>%
     dplyr::mutate(lag_time = floor(lag_time/window_size)) %>%
     dplyr::ungroup() %>%
-    dplyr::mutate(variable = stringr::str_replace_all(variable, "\\d+",paste("lag",lag_time,sep ="_"))) %>%
-    dplyr::select(-lead_value,-lag_time)
+    dplyr::mutate(variable = stringr::str_replace_all(variable, "\\d+",paste(lag_compute,lag_time,sep ="_"))) %>%
+    dplyr::group_by(encounter_id,variable) %>% 
+    dplyr::mutate(new_value = shift(new_value, n = 1,type = "lag")) %>% 
+   # dplyr::mutate(new_value = coalesce(value,new_value)) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::select(-lead_value,-lag_time,-value,-lag_compute) %>% 
+    dplyr::rename(value = new_value)
 
 
   return(lag_frame)
