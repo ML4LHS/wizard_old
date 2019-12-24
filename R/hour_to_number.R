08c89d3e09fd9054f8519cb344bcddd663d712e5
Adharsh Murali <33362260+adharshmurali@users.noreply.github.com>
2019-12-24 13:01:53 -0500

    Support to convert list of period object to number

--- R/hour_to_number.R
+++ R/hour_to_number.R
@@ -5,10 +5,11 @@
 #' @param window_size window size
 #' @param lookback lookback
 #' @param lookahead lookahead
+#' @param step step
 #' @return ist of values
 #' @export
 
-hour_to_number = function(window_size = NULL,lookback =NULL,lookahead = NULL){
+hour_to_number = function(window_size = NULL,lookback =NULL,lookahead = NULL, step = NULL){
 
   # if( fixed_data %>% distinct(encounter_id)%>% nrow()  !=  temporal_data %>%  distinct(encounter_id) %>% nrow() ){
   #   warning("Mismatch in encounter id's between the fixed and temporal data. You may lose some information")
@@ -29,115 +30,179 @@ hour_to_number = function(window_size = NULL,lookback =NULL,lookahead = NULL){
   #   mutate(time = time_length(interval(admit_time,time), units))
   #
 
-  if (!is.null(window_size) & !is.null(lookback) & !is.null(lookahead)){
+  if (!is.null(window_size) & !is.null(lookback) & !is.null(lookahead) & !is.null(step) ){
 
-    object = list(window_size,lookback,lookahead)
+    
+    # Identifying the units of step function 
+    if( tolower(class(step)[[1]]) == "period")
+    {
+    
     units = ""
-
-    period_list = lapply(object,function(a)
-    {  if (tolower(class(a)[1]) == "period"){
-      time_list = stringr::str_split(a[[1]], " ")
-
-      for ( x in range(1, length(time_list[[1]])))
-      {
-        if(as.numeric(stringr::str_extract_all(time_list[[1]][1],"\\d+")) > 0){
-          if (length(time_list[[1]]) == 3){
-            if(x == 1){
-              units = "hours"
-              break
-            }
-            if(x == 2){
-              units = "minutes"
-              break
-            }
-            if(x == 3){
-              units = "seconds"
-              break
-            }
-          }
-          else{
-            if(x == 1){
-              units = "years"
-              break
-            }
-            if(x == 2){
-              units = "months"
-              break
-            }
-            if(x == 3){
-              units = "days"
-              break
-            }
-            if(x == 4){
-              units = "hours"
-              break
-            }
-            if(x == 5){
-              units = "minutes"
-              break
-            }
-            if(x == 6){
-              units = "seconds"
-              break
-            }
-          }
-        }
-      }
-      # temporal_data = temporal_data %>%
-      #   mutate(admit_time = parse_date_time(admit_time,
-      #                                       orders = c("ymd_HMS","dmy_HMS","mdy_HMS"))) %>%
-      #   mutate(time = time_length(interval(admit_time,time), units))
+   time_list = stringr::str_split(step," ")
+   for ( x in range(1, length(time_list[[1]])))
+   {
+     if(as.numeric(stringr::str_extract_all(time_list[[1]][1],"\\d+")) > 0){
+       if (length(time_list[[1]]) == 3){
+         if(x == 1){
+           units = "hours"
+           break
+         }
+         if(x == 2){
+           units = "minutes"
+           break
+         }
+         if(x == 3){
+           units = "seconds"
+           break
+         }
+       }
+       else{
+         if(x == 1){
+           units = "years"
+           break
+         }
+         if(x == 2){
+           units = "months"
+           break
+         }
+         if(x == 3){
+           units = "days"
+           break
+         }
+         if(x == 4){
+           units = "hours"
+           break
+         }
+         if(x == 5){
+           units = "minutes"
+           break
+         }
+         if(x == 6){
+           units = "seconds"
+           break
+         }
+       }
+     }
+   }
+     
     }
-      #print(units)
-      #}
-      units})
+    else{
+      stop(" The step variable should be a valid period object.")
+    }
+  
+
+    # object = list(window_size,lookback,lookahead)
+    # units = ""
+    # 
+    # period_list = lapply(object,function(a)
+    # {  if (tolower(class(a)[1]) == "period"){
+    #   time_list = stringr::str_split(a[[1]], " ")
+    # 
+    #   for ( x in range(1, length(time_list[[1]])))
+    #   {
+    #     if(as.numeric(stringr::str_extract_all(time_list[[1]][1],"\\d+")) > 0){
+    #       if (length(time_list[[1]]) == 3){
+    #         if(x == 1){
+    #           units = "hours"
+    #           break
+    #         }
+    #         if(x == 2){
+    #           units = "minutes"
+    #           break
+    #         }
+    #         if(x == 3){
+    #           units = "seconds"
+    #           break
+    #         }
+    #       }
+    #       else{
+    #         if(x == 1){
+    #           units = "years"
+    #           break
+    #         }
+    #         if(x == 2){
+    #           units = "months"
+    #           break
+    #         }
+    #         if(x == 3){
+    #           units = "days"
+    #           break
+    #         }
+    #         if(x == 4){
+    #           units = "hours"
+    #           break
+    #         }
+    #         if(x == 5){
+    #           units = "minutes"
+    #           break
+    #         }
+    #         if(x == 6){
+    #           units = "seconds"
+    #           break
+    #         }
+    #       }
+    #     }
+    #   }
+    #   # temporal_data = temporal_data %>%
+    #   #   mutate(admit_time = parse_date_time(admit_time,
+    #   #                                       orders = c("ymd_HMS","dmy_HMS","mdy_HMS"))) %>%
+    #   #   mutate(time = time_length(interval(admit_time,time), units))
+    # }
+    #   #print(units)
+    #   #}
+    #   units})
 
 
-    if (period_list %>% unique() %>% length == 1 & period_list[[1]]!=""){
-      units = period_list[[1]]
+  
       if(units != ""){
 
         if (units == "years"){
-          window_size = lubridate::period_to_seconds(window_size)/(3600*24*7*365)
-          lookback = lubridate::period_to_seconds(lookback)/(3600*24*7*365)
-          lookahead = lubridate::period_to_seconds(lookahead)/(3600*24*7*365)
+          window_size = lapply(window_size,function(x) {lubridate::period_to_seconds(x)/(3600*24*7*365)})
+          lookback = lapply(lookback,function(x) {lubridate::period_to_seconds(x)/(3600*24*7*365)})
+          lookahead =lubridate::period_to_seconds(lookahead)/(3600*24*7*365)
         }
 
         if (units == "weeks"){
-          window_size = lubridate::period_to_seconds(window_size)/(3600*24*7)
-          lookback = lubridate::period_to_seconds(lookback)/(3600*24*7)
+          window_size = lapply(window_size, function(x) {lubridate::period_to_seconds(x)/(3600*24*7)})
+          lookback = lapply(lookback, function(x) {lubridate::period_to_seconds(x)/(3600*24*7)})
           lookahead = lubridate::period_to_seconds(lookahead)/(3600*24*7)
         }
         if (units == "days"){
-          window_size = lubridate::period_to_seconds(window_size)/(3600*24)
-          lookback = lubridate::period_to_seconds(lookback)/(3600*24)
+          window_size = lapply(window_size,function(x) {lubridate::period_to_seconds(x)/(3600*24)})
+          lookback = lapply(lookback, function(x) { lubridate::period_to_seconds(x)/(3600*24)})
           lookahead = lubridate::period_to_seconds(lookahead)/(3600*24)
         }
 
         if (units == "hours"){
-          window_size = lubridate::period_to_seconds(window_size)/3600
-          lookback = lubridate::period_to_seconds(lookback)/(3600)
+          window_size = lapply(window_size, function(x) {lubridate::period_to_seconds(x)/3600})
+          lookback = lapply(lookback, function(x) {lubridate::period_to_seconds(x)/(3600)})
           lookahead = lubridate::period_to_seconds(lookahead)/(3600)
         }
         if (units == "mins"){
-          window_size = lubridate::period_to_seconds(window_size)/60
-          lookback = lubridate::period_to_seconds(lookback)/(60)
+          window_size = lapply( window_size, function(x){lubridate::period_to_seconds(x)/60})
+          lookback = lapply(lookback, function(x) {lubridate::period_to_seconds(x)/(60)})
           lookahead = lubridate::period_to_seconds(lookahead)/(60)
         }
         if (units == "secs"){
-          window_size = lubridate::period_to_seconds(window_size)
-          lookback = lubridate::period_to_seconds(lookback)
+          window_size = lapply(window_size, function(x) {lubridate::period_to_seconds(x)})
+          lookback = lapply(lookback, function(x) {lubridate::period_to_seconds(x)})
           lookahead = lubridate::period_to_seconds(lookahead)
         }
       }
-    }
     else{
-      stop(" The period objects provided in the arguments are not of same units.")
+      stop("Please validate the data type of the step argument.")
     }
+    
   }
   else{
-    stop("Please provide the lookback lookahead and the window_size objects")
+    stop("Please fill in the lookback,lookahead,window size,and step arguments of the function.")
   }
 
   return(list("units" = units, "lookback" = lookback, "lookahead" = lookahead , "window_size" = window_size))
 }
+
+
+
+
+
+
