3a7b0ed49863040b463dc8abfa3c04ea981bdca2
Adharsh Murali <adharsh@umich.edu>
2020-02-04 11:37:26 -0500

    Adding lag features to the diff function

--- R/diff_feature.R
+++ R/diff_feature.R
@@ -15,18 +15,18 @@ diff_feature = function(.x,.y,window_size, lag_compute){
   lag_frame = .x %>%
     dplyr::group_by(encounter_id,variable) %>%
     arrange(encounter_id,time) %>%
-    dplyr::mutate(lead_value = data.table::shift(value,n =1,type = "lead") ) %>%
+    dplyr::mutate(lag_value = data.table::shift(value,n =1,type = "lag") ) %>%
+    dplyr::mutate_at(vars(lag_value), as.numeric) %>% 
+    ungroup() %>% 
     dplyr::mutate(lag_compute = case_when(lag_compute == "both" ~ "prop/diff", T~ lag_compute)) %>% 
     separate_rows(lag_compute,convert = T, sep = "/") %>% 
     dplyr::mutate(new_value = ifelse(lag_compute == "prop",lead_value / value, lead_value - value)) %>% 
     #ungroup() %>%
     dplyr::mutate(lag_time = str_extract_all(variable, "\\d+")) %>%
-    tidyr::unnest() %>%
     dplyr::mutate_at(vars(lag_time), as.numeric) %>%
-    #dplyr::mutate(lag_time = lag_time - min(lag_time, na.rm = T)) %>%
     dplyr::filter(lag >= 0) %>%
     dplyr::mutate(lag_time = floor(lag_time/window_size)) %>%
-    dplyr::ungroup() %>%
+    #dplyr::ungroup() %>%
     dplyr::mutate(variable = stringr::str_replace_all(variable, "\\d+",paste(lag_compute,lag_time,sep ="_"))) %>%
    # dplyr::group_by(encounter_id,variable) %>% 
     #dplyr::mutate(new_value = data.table::shift(new_value, n = 1,type = "lag")) %>% 
